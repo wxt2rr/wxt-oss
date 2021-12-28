e06e0646c3f1460e52d059a9b6f14979c44dbaaa
wangxt <wangxt@ckt.cn>
2021-12-28 11:22:32 +0800

    init project

---
+++ src/main/java/com/wangxt/oss/WxtOssApplication.java
@@ -0,0 +1,13 @@
+package com.wangxt.oss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxtOssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WxtOssApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wangxt/oss/WxtOssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wangxt.oss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WxtOssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
